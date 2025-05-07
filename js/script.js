843487a8bfd950529f1de471d4696485340c23d6
Lawliet464 <lutherarthurhawkins@gmail.com>
2025-05-07 14:31:52 +0100

    dev web

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Exemple de script JavaScript simple
+
+// Message de bienvenue dans la console
+console.log("Bienvenue sur le site de la CAN 2025 !");
+
+// Vous pouvez ajouter ici des interactions JS supplémentaires
